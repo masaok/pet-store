45fd43d4f130596ba7e49b4cc1d7c52790052e25
Masao Kitamura <masaok@gmail.com>
2019-05-28 19:59:49 -0700

    Added Dogs fetch API and rendered to screen

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,14 +1,44 @@
 import React from 'react'
 class Dogs extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({ data })
+      });
+  }
+
   render() {
     const { match } = this.props
+    // const { message } = this.state.data
+
+    console.log("DOGS > RENDER > state:")
+    console.log(this.state)
 
     const breed = match.params.breed
+
+    // console.log(message)
 
     return (
       <div>
         <h1>Dogs</h1>
         <h1>Breed: {breed}</h1>
+        <h1>Random image:</h1>
+        <h1><img src={this.state.data && this.state.data.message}
+                 alt={this.state.data && this.state.data.message} /></h1>
+        <h2>{this.state.data && this.state.data.message}</h2>
       </div>
     )
   }
