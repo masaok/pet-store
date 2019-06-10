99eae9278d677333482fe7f7653b5a991c34c6db
Masao Kitamura <masaok@gmail.com>
2019-06-09 20:55:12 -0700

    Updates

---
+++ precache-manifest.36352583b9fe31c8d562942e0f4b03f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d079a8fc6f21995a452cf658dde8fa8",
+    "url": "/pet-store/index.html"
+  },
+  {
+    "revision": "00f9c07d5bd6cffbd3aa",
+    "url": "/pet-store/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3b0799750105e96f1b1b",
+    "url": "/pet-store/static/js/2.61c84bad.chunk.js"
+  },
+  {
+    "revision": "00f9c07d5bd6cffbd3aa",
+    "url": "/pet-store/static/js/main.62829adb.chunk.js"
+  },
+  {
+    "revision": "722099ab406cc33beaa2",
+    "url": "/pet-store/static/js/runtime~main.fe989b93.js"
+  }
+]);
