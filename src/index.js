13686499e7db03b2dbc943fb173ce282b11aa13b
Masao Kitamura <masaok@gmail.com>
2019-06-09 18:51:39 -0700

    Added Theme override

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+/**
+ * Boilerplate React App stuff
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * Routing stuff
+ */
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+/**
+ * Material UI stuff
+ */
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+/**
+ * Custom components
+ */
 import Dogs from './components/Dogs'
 import Users from './components/Users'
 import Contact from './components/Contact'
@@ -17,29 +32,46 @@ import GlobalHeader from './components/shared/GlobalHeader'
 
 import NotFound from './components/errors/NotFound'
 
+// https://material-ui.com/customization/themes/#createmuitheme-options-theme
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: '#00ff00' // overwrite secondary.main to green
+    },
+  },
+})
+
 const routing = (
-  <Router>
-    <div>
-      <Switch>
-        <Route path="/" component={GlobalHeader} />
-      </Switch>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route exact path="/dogs" component={Dogs} />
-        <Route exact path="/dogs/:breed" component={Dogs} />
-        <Route exact path="/users" component={Users} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/starfish" component={Starfish} />
-        <Route component={NotFound} />
-      </Switch>
-      <Switch>
-        <Route exact path="/" component={Footer} title="Home" />
-      </Switch>
-      <Switch>
-        <Route path="/" component={GlobalFooter} />
-      </Switch>
-    </div>
-  </Router>
+  // https://reactjs.org/docs/fragments.html
+  <React.Fragment>
+    {/* https://material-ui.com/components/css-baseline/ */}
+    <CssBaseline />
+    {/* https://material-ui.com/customization/themes/#theme-provider */}
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" component={GlobalHeader} />
+          </Switch>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route exact path="/dogs" component={Dogs} />
+            <Route exact path="/dogs/:breed" component={Dogs} />
+            <Route exact path="/users" component={Users} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/starfish" component={Starfish} />
+            <Route component={NotFound} />
+          </Switch>
+          <Switch>
+            <Route exact path="/" component={Footer} title="Home" />
+          </Switch>
+          <Switch>
+            <Route path="/" component={GlobalFooter} />
+          </Switch>
+        </div>
+      </Router>
+    </MuiThemeProvider>
+  </React.Fragment>
 )
 ReactDOM.render(routing, document.getElementById('root'))
 
